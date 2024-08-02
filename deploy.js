ecc2648999825505a1f1ff7ba16b0cb54e992386
Ibrahima Sylla <isyll711@gmail.com>
2024-08-02 21:21:17 +0000

    fix

--- deploy.js
+++ deploy.js
@@ -1,8 +1,14 @@
 import { publish } from 'gh-pages'
 
 console.log(' Deploying...\n')
-publish('dist', (err) =>
-  err
-    ? console.error('An error was encountered\n\n' + err)
-    : console.log(' Deployed successfully\n'),
+publish(
+  'dist',
+  {
+    branch: 'gh-pages',
+    repo: 'https://github.com/isyll/isyll.github.io.git',
+  },
+  (err) =>
+    err
+      ? console.error('An error was encountered\n\n' + err)
+      : console.log(' Deployed successfully\n'),
 )
