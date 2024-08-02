b44ff2a20fa9184b34723d75f20b571b09880928
Ibrahima Sylla <isyll711@gmail.com>
2024-08-02 21:28:02 +0000

    deploy

--- deploy.js
+++
@@ -1,8 +0,0 @@
-import { publish } from 'gh-pages'
-
-console.log(' Deploying...\n')
-publish('dist', (err) =>
-  err
-    ? console.error('An error was encountered\n\n' + err)
-    : console.log(' Deployed successfully\n'),
-)
--- src/components/Logo.tsx
+++
@@ -1,5 +0,0 @@
-function Logo() {
-  return <h1 className='text-xxl font-bold'>Isyll</h1>
-}
-
-export default Logo
