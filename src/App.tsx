919eabc3d902d06c133292fe8f3c6deba421cf53
Ibrahima Sylla <isyll711@gmail.com>
2024-09-15 14:20:07 +0000

    init new version

--- src/App.tsx
+++ src/App.tsx
@@ -1,30 +1,5 @@
-import { About } from './components/About'
-import Contact from './components/Contact'
-import Footer from './components/Footer'
-import Header from './components/Header'
-import Hero from './components/Hero'
-import MyProjects from './components/MyProjects'
-import Sidenav from './components/Sidenav'
-import Skills from './components/Skills'
-
 function App() {
-  return (
-    <>
-      <div className='flex'>
-        <Sidenav />
-        <main className='ml-[50px]' id='home'>
-          <Header />
-          <Hero />
-          <About />
-          <Skills />
-          <MyProjects />
-          <Contact />
-          <div className='h-[200px]'></div>
-          <Footer />
-        </main>
-      </div>
-    </>
-  )
+  return <></>
 }
 
 export default App
--- src/components/Overlay.tsx
+++
@@ -1,9 +0,0 @@
-import { useLockBodyScroll } from '@uidotdev/usehooks'
-
-function Overlay({ onClick }: { onClick?: () => void }) {
-  useLockBodyScroll()
-
-  return <div className='fixed inset-0 z-20 opacity-55 bg-black' onClick={onClick} />
-}
-
-export default Overlay
